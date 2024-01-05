a7f64a78c2d1708bf61a343e2c94c3e567f87042
arielrecto <--add>
2024-01-06 03:37:02 +0800

    update:hide login&signup when user is auth

--- resources/views/components/landing-page/navbar.blade.php
+++ resources/views/components/landing-page/navbar.blade.php
@@ -2,9 +2,10 @@
     <div class="flex flex-col justify-center gap-2 md:flex-row lg:gap-2">
         <div class="w-full md:w-10 md:h-10 flex justify-center">
             <img src="{{ asset('image/logo.png') }}" alt="" srcset=""
-            class="h-5 w-5 md:h-full md:w-full rounded-full object object-center">
+                class="h-5 w-5 md:h-full md:w-full rounded-full object object-center">
         </div>
-        <p class="capitalize text-xs lg:text-base text-center font-semibold flex flex-col md:flex-row md:items-center text-base-100 gap-2">
+        <p
+            class="capitalize text-xs lg:text-base text-center font-semibold flex flex-col md:flex-row md:items-center text-base-100 gap-2">
             <span class="uppercase">Doc aid </span> <span class="hidden md:flex">diagnostic Center & medical clinic</span>
         </p>
     </div>
@@ -32,12 +33,23 @@
         </h1> --}}
     </div>
     <div class="flex items-center gap-2">
-        <a href="{{ route('login') }}"
-            class="text-base-100 font-thin capitalize text-xs md:text-sm hover:font-bold duration-700 px-2">
-            login
-        </a>
-        <a href="{{ route('register') }}" class="btn btn-xs md:btn-sm btn-secondary font-bold text-base-100 capitalize">
-            Signup
-        </a>
+        @auth
+
+            <a href="{{ route('home') }}" class="btn btn-xs md:btn-sm btn-secondary font-bold text-base-100 capitalize">
+                Dashboard
+            </a>
+
+        @endauth
+
+        @guest
+            <a href="{{ route('login') }}"
+                class="text-base-100 font-thin capitalize text-xs md:text-sm hover:font-bold duration-700 px-2">
+                login
+            </a>
+            <a href="{{ route('register') }}" class="btn btn-xs md:btn-sm btn-secondary font-bold text-base-100 capitalize">
+                Signup
+            </a>
+        @endguest
+
     </div>
 </div>
