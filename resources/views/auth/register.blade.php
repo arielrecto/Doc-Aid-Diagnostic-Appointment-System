cb503b0773c5d913d7813f25765c03e7e44de33c
arielrecto <--add>
2024-01-20 05:26:45 +0800

    update:password show

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,14 +5,16 @@
         <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Username')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
+                autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -20,10 +22,20 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
+            <div class="w-full relative" x-data="{ showPassword: false }">
+                <input type="password" name="password" x-bind:type="showPassword ? 'text' : 'password'"
+                class='border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full' required autocomplete="new-password">
+                <button @click.prevent="showPassword = !showPassword" class="btn btn-xs btn-ghost absolute top-2 right-1">
+                    <i class="fi fi-rr-eye"></i>
+                </button>
+            </div>
+
+
+            {{--
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
                             name="password"
-                            required autocomplete="new-password" />
+                            required autocomplete="new-password" /> --}}
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
             <div class="flex flex-col gap-2">
@@ -38,15 +50,25 @@
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+
+            <div class="w-full relative" x-data="{ showPassword: false }">
+                <input type="password"name="password_confirmation" x-bind:type="showPassword ? 'text' : 'password'"
+                class='border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full' required autocomplete="new-password">
+                <button @click.prevent="showPassword = !showPassword" class="btn btn-xs btn-ghost absolute top-2 right-1">
+                    <i class="fi fi-rr-eye"></i>
+                </button>
+            </div>
+
+
+            {{-- <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                name="password_confirmation" required autocomplete="new-password" /> --}}
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
 
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,6 +13,9 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+            <!-- ICONS -->
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'>
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
